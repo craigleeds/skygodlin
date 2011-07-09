1aef17d30b2569532462ec00bb74475cbc016d19
Frederick Schoeneman <fred.schoeneman@gmail.com>
2011-07-08 23:12:09 -0700

    added variables

--- config/email.rb
+++ config/email.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => "smtp.sendgrid.net",
   :port           => "25",
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => ENV['SENDGRID_DOMAIN']
+  :user_name      => ENV['app588483@heroku.com'],
+  :password       => ENV['c6f9e8b604f68300e1'],
+  :domain         => ENV['www.skygodlin.com']
 }
 
