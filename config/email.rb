bfb922558eca222ccad3667f1142834a6b33ccad
Frederick Schoeneman <fred.schoeneman@gmail.com>
2011-07-10 19:15:15 -0700

    moved smtp config to config/environments/production.rb

--- config/email.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address        => "smtp.sendgrid.net",
-  :port           => "25",
-  :authentication => :plain,
-  :user_name      => ENV['app588483@heroku.com'],
-  :password       => ENV['c6f9e8b604f68300e1'],
-  :domain         => ENV['www.skygodlin.com']
-}
-
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,14 @@ Skygodlin::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
+  ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['app588483@heroku.com'],
+  :password       => ENV['c6f9e8b604f68300e1'],
+  :domain         => ENV['www.skygodlin.com']
+}
 
   # Disable Rails's static asset server (Apache or nginx will
   # already do this)
