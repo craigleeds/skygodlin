3a6211e6895ccfbacd6e3cf6999f44697070b77a
Frederick Schoeneman <fred.schoeneman@gmail.com>
2011-07-17 23:24:14 -0700

    added application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def name
+    user = @user.email.split('@')
+    local_part = user[0]
+    if @name.nil?
+      local_part
+    else
+      @name
+    end
+  end
 end
+
